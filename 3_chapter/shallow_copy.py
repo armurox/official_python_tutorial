8e186cca3e1f9e5e83eb29c8c7ca685c8257597e
Arman Jasuja <arman_jasuja@yahoo.com>
2026-02-07 22:53:10 +0400

    Updated shallow copy example to show proof that it is a shallow copy

--- 3_chapter/shallow_copy.py
+++ 3_chapter/shallow_copy.py
@@ -7,3 +7,11 @@ print(rgba, rgba)  # both will have Alph
 correct_rgba = rgba[:]  # shallow copy
 correct_rgba[-1] = 'Alpha' # only updates itself, since it is a shallow copy
 print(correct_rgba, rgba)
+
+test_variable = {'Red': 'test'}
+shallow_copy_proof = [test_variable, 1]
+shallow_copy_proof_copy = shallow_copy_proof[:]
+shallow_copy_proof_copy[-1] = 2
+print(shallow_copy_proof, shallow_copy_proof_copy)
+shallow_copy_proof[0]['Green'] = 'new_test'  # the original test_variable is updated itself, so all references to it will change
+print(shallow_copy_proof, shallow_copy_proof_copy)
