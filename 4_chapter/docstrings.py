c03bc3e51cc0a676b6c20c2cf7b7dec9761bdb35
Arman Jasuja <arman_jasuja@yahoo.com>
2026-02-08 15:51:29 +0400

    Accessing docstrings

---
+++ 4_chapter/docstrings.py
@@ -0,0 +1,10 @@
+def my_function():
+    """ Do nothing, but illustrate the documentation
+    
+    This function truly does nothing:
+        >>>> my_function()
+        >>>>
+    """
+    pass
+
+print(my_function.__doc__)
